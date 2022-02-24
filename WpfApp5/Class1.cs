4c82bafd18f4ae0791316081bd6f2e141de3502f
Tetris <Tetris@DESKTOP-1CVAJIM>
2022-02-24 17:11:18 +0300

    Добавьте файлы проекта.

---
+++ WpfApp5/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    class Class1
+    {
+        public static hhEntities con0bj;
+
+    }
+}
---
+++ WpfApp5/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    class Class2
+    {
+        public static Class2 Class2Main;
+    }
+}
